d3a9a35b21264f7dddad51d697c59c5e81f14d70
Lex <alexisgabriel.lu@gmail.com>
2021-11-12 14:59:11 -0500

    Creado Logo

---
+++ AppAutoLAvado/Login.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppAutoLAvado
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            /*MainPage = new NavigationPage(new Login());*/
+        }
+
+
+        private async void btnRegistrate_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Registrate());
+        }
+    }
+}
